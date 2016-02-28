4e3f8526832fa23568f53680d05dc58b57c321d7
Vadym Markov <markov.vadym@gmail.com>
2016-02-28 19:48:31 +0100

    Introduce networking class

--- Sources/Networking.swift
+++ Sources/Networking.swift
@@ -1,9 +1,18 @@
-//
-//  Networking.swift
-//  Malibu
-//
-//  Created by Vadym Markov on 28/02/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
-
 import Foundation
+
+public class Networking {
+  
+  enum SessionTaskKind {
+    case Data, Upload, Download
+  }
+  
+  let sessionConfiguration: SessionConfiguration
+  
+  lazy var session: NSURLSession = {
+    return NSURLSession(configuration: self.sessionConfiguration.configuration)
+  }()
+  
+  public init(sessionConfiguration: SessionConfiguration = .Default) {
+    self.sessionConfiguration = sessionConfiguration
+  }
+}
