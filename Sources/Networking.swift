ba382ee9761c7c23f740d2a8ec3bfb56266d57f6
Vadym Markov <markov.vadym@gmail.com>
2016-02-29 09:39:14 +0100

    Don't add accept

--- Sources/Networking.swift
+++ Sources/Networking.swift
@@ -7,47 +7,46 @@ public enum Error: ErrorType {
 }
 
 public class Networking {
-  
+
   enum SessionTaskKind {
     case Data, Upload, Download
   }
-  
+
   let sessionConfiguration: SessionConfiguration
-  
+
   lazy var session: NSURLSession = {
     return NSURLSession(configuration: self.sessionConfiguration.configuration)
   }()
-  
+
   public init(sessionConfiguration: SessionConfiguration = .Default) {
     self.sessionConfiguration = sessionConfiguration
   }
-  
+
   func request(method: Method, URL: NSURL, contentType: ContentType, parameters: [String: AnyObject]?) throws -> Promise<NSData> {
     let promise = Promise<NSData>()
-    
+
     let request = NSMutableURLRequest(URL: URL)
     request.HTTPMethod = method.rawValue
     request.addValue(contentType.value, forHTTPHeaderField: "Content-Type")
-    request.addValue("application/json", forHTTPHeaderField: "Accept")
-    
+
     if let encoder = parameterEncoders[contentType], let parameters = parameters {
       request.HTTPBody = try encoder.encode(parameters)
     }
-    
+
     session.dataTaskWithRequest(request, completionHandler: { data, response, error in
       if let error = error {
         promise.reject(error)
         return
       }
-      
+
       guard let data = data else {
         promise.reject(Error.NoDataInResponse)
         return
       }
-      
+
       promise.resolve(data)
     }).resume()
-    
+
     return promise
   }
 }
