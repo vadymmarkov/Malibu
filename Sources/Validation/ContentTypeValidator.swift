0f61249cd1114806ad3a1f25e7ee4f30c6f7d5d6
Vadym Markov <markov.vadym@gmail.com>
2016-03-03 02:24:04 +0100

    Content type validator

--- Sources/Validation/ContentTypeValidator.swift
+++ Sources/Validation/ContentTypeValidator.swift
@@ -1,9 +1,37 @@
-//
-//  ContentTypeValidator.swift
-//  Malibu
-//
-//  Created by Vadym Markov on 03/03/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
-
 import Foundation
+
+public struct ContentTypeValidator<T : SequenceType where T.Generator.Element == String>: Validating {
+  
+  public var contentTypes: T
+  
+  public func validateResponse(response: NSHTTPURLResponse) throws {
+    if let responseContentType = response.MIMEType, responseMIMEType = MIMEType(contentType: responseContentType) {
+      contentTypes.forEach {
+        if MIMEType(contentType: $0)?.matches(responseMIMEType) == true {
+          return
+        }
+      }
+    } else {
+      contentTypes.forEach {
+        let expectedMIMEType = MIMEType(contentType: $0)
+        
+        if expectedMIMEType?.type == "*" && expectedMIMEType?.subtype == "*" {
+          return
+        }
+      }
+    }
+      
+    var error: ErrorType
+      
+    if let responseContentType = response.MIMEType {
+      error = Error.UnacceptableContentType(responseContentType)
+    } else {
+      error = Error.MissingContentType
+    }
+      
+    throw error
+  }
+}
+
+
+
