27989c549a66762e3400855966b7e83bee5157c7
Vadym Markov <markov.vadym@gmail.com>
2016-03-03 02:23:42 +0100

    Validating protocol

--- Sources/Validation/Validating.swift
+++ Sources/Validation/Validating.swift
@@ -1,9 +1,39 @@
-//
-//  Validating.swift
-//  Malibu
-//
-//  Created by Vadym Markov on 03/03/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
-
 import Foundation
+import When
+
+public typealias Validation = (response: NSHTTPURLResponse) -> ErrorType?
+
+public protocol Validating {
+  func validateResponse(response: NSHTTPURLResponse) throws
+}
+
+extension Validating {
+  
+  func validate(response: Response) -> Response {
+    let validationResponse = Response(request: response.request, response: response.response)
+    let validate = validateResponse
+    
+    response.done({ data in
+      guard let response = response.response else {
+        let error =  Error.NoResponseReceived
+        validationResponse.reject(error)
+        return
+      }
+      
+      do {
+        try validate(response)
+      } catch {
+        validationResponse.reject(error)
+        return
+      }
+      
+      validationResponse.resolve(data)
+    })
+    
+    response.fail({ error in
+      validationResponse.reject(error)
+    })
+    
+    return validationResponse
+  }
+}
