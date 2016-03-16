a92eb2a5cd69aa06c666e3d0324f72ffc55c3ce8
Vadym Markov <markov.vadym@gmail.com>
2016-03-16 23:15:45 +0100

    Add networkings

--- Sources/Malibu.swift
+++ Sources/Malibu.swift
@@ -1,8 +1,25 @@
 import Foundation
 
+// MARK: - Helpers
+
+var methodsWithEtags: [Method] = [.GET, .PATCH, .PUT]
+var networkings = [String: Networking]()
+
+// MARK: - Public
+
+public var networking = Networking()
+
 public var parameterEncoders: [ContentType: ParameterEncoding] = [
   .JSON: JSONParameterEncoder(),
   .FormURLEncoded: FormURLEncoder()
 ]
 
-var methodsWithEtags: [Method] = [.GET, .PATCH, .PUT]
+public func register(name: String, networking: Networking) {
+  networkings[name] = networking
+}
+
+public func networkingNamed(name: String) -> Networking {
+  return networkings[name] ?? networking
+}
+
+
