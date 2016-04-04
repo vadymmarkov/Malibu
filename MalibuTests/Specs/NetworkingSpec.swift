f30f68f7ad482003c4a570c8a69ac3033d2a2d1d
Vadym Markov <markov.vadym@gmail.com>
2016-04-04 13:52:33 +0200

    Add tests

--- MalibuTests/Specs/NetworkingSpec.swift
+++ MalibuTests/Specs/NetworkingSpec.swift
@@ -36,6 +36,26 @@ class NetworkingSpec: QuickSpec {
           expect(networking.mocks["GET http://hyper.no"] === mock).to(beTrue())
         }
       }
+
+      describe("#prepareMock") {
+        it("runs beforeEach closure on mocked request and returns registered mock") {
+          let request = GETRequest()
+          let mock = Mock(request: request, response: nil, data: nil, error: nil)
+
+          networking.register(mock: mock)
+          networking.beforeEach = { (request: Requestable) in
+            var request = request
+            request.message.parameters["test"] = true
+
+            return request
+          }
+
+          let result = networking.prepareMock(request)
+
+          expect(result).toNot(beNil())
+          expect(result?.request.message.parameters["test"] as? Bool).to(beTrue())
+        }
+      }
     }
   }
 }
