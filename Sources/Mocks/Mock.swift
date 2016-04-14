3f68b4e690cf0483e3d05eb91c6854cbffee3fa8
Vadym Markov <markov.vadym@gmail.com>
2016-04-14 20:28:41 +0200

    Convenience init with file name

--- Sources/Mocks/Mock.swift
+++ Sources/Mocks/Mock.swift
@@ -15,4 +15,18 @@ public class Mock {
     self.response = response
     self.error = error
   }
+
+  public convenience init(request: Requestable, fileName: String, bundle: NSBundle = NSBundle.mainBundle()) {
+    guard let fileURL = NSURL(string: fileName),
+      resource = fileURL.URLByDeletingPathExtension?.absoluteString,
+      filePath = bundle.pathForResource(resource, ofType: fileURL.pathExtension),
+      data = NSData(contentsOfFile: filePath),
+      response = NSHTTPURLResponse(URL: fileURL, statusCode: 200, HTTPVersion: "HTTP/2.0", headerFields: nil)
+      else {
+        self.init(request: request, response: nil, data: nil, error: Error.NoResponseReceived)
+        return
+    }
+
+    self.init(request: request, response: response, data: data, error: nil)
+  }
 }
