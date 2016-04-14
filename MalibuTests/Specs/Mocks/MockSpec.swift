f451e991ad1228a389952af551bdd0be112cd022
Vadym Markov <markov.vadym@gmail.com>
2016-04-14 20:46:23 +0200

    Add tests

--- MalibuTests/Specs/Mocks/MockSpec.swift
+++ MalibuTests/Specs/Mocks/MockSpec.swift
@@ -9,12 +9,12 @@ class MockSpec: QuickSpec {
     describe("Mock") {
       var mock: Mock!
       let request = GETRequest()
-      let response = NSHTTPURLResponse(URL: NSURL(string: "http://hyper.no")!,
+      var response = NSHTTPURLResponse(URL: NSURL(string: "http://hyper.no")!,
         statusCode: 200, HTTPVersion: "HTTP/2.0", headerFields: nil)!
       let data = "test".dataUsingEncoding(NSUTF32StringEncoding)
       let error = Error.NoDataInResponse
 
-      describe("#init") {
+      describe("#init:request:response:data:error") {
         beforeEach {
           mock = Mock(request: request, response: response, data: data, error: error)
         }
@@ -24,6 +24,28 @@ class MockSpec: QuickSpec {
           expect(mock.response).to(equal(response))
           expect(mock.data).to(equal(data))
           expect(mock.error as! Error == error).to(beTrue())
+        }
+      }
+
+      describe("#init:request:fileName:bundle") {
+        let fileName = "mock.json"
+
+        beforeEach {
+          mock = Mock(request: request, fileName: fileName, bundle: NSBundle(forClass: MockSpec.self))
+          response = NSHTTPURLResponse(URL: NSURL(string: fileName)!, statusCode: 200,
+            HTTPVersion: "HTTP/2.0", headerFields: nil)!
+        }
+
+        it("sets properties") {
+          let serializer = JSONSerializer()
+          let dictionary = try! serializer.serialize(mock.data!, response: response) as! [String: String]
+
+          expect(mock.request.message).to(equal(request.message))
+          expect(mock.response?.statusCode).to(equal(response.statusCode))
+          expect(mock.error).to(beNil())
+          expect(dictionary["first_name"]).to(equal("John"))
+          expect(dictionary["last_name"]).to(equal("Hyperseed"))
+          expect(dictionary["email"]).to(equal("ios@hyper.no"))
         }
       }
     }
