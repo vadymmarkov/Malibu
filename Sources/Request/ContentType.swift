777df14cc923a700e60c5115e83508571ed62cef
Vadym Markov <markov.vadym@gmail.com>
2016-05-02 16:36:41 +0200

    Move some logic to content type

--- Sources/Request/ContentType.swift
+++ Sources/Request/ContentType.swift
@@ -1,7 +1,10 @@
+import Foundation
+
 public enum ContentType {
   case Query
   case FormURLEncoded
   case JSON
+  case MultipartFormData(String)
   case Custom(String)
 
   var header: String? {
@@ -14,11 +17,30 @@ public enum ContentType {
       string = "application/json"
     case .FormURLEncoded:
       string = "application/x-www-form-urlencoded"
+    case .MultipartFormData(let boundary):
+      string = "multipart/form-data; boundary=MalibuBoundary-\(boundary)"
     case .Custom(let value):
       string = value
     }
 
     return string
+  }
+
+  var encoder: ParameterEncoding? {
+    var encoder: ParameterEncoding?
+
+    switch self {
+    case .JSON:
+      encoder = JSONParameterEncoder()
+    case .FormURLEncoded:
+      encoder = FormURLEncoder()
+    case .MultipartFormData(let boundary):
+      encoder = MultipartFormEncoder(boundary: boundary)
+    default:
+      break
+    }
+
+    return encoder
   }
 }
 
