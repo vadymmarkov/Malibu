f1d697870a7c3f8ff48011fda7cbdfc54e2ccc14
Vadym Markov <markov.vadym@gmail.com>
2016-05-13 12:15:33 +0200

    Implement error and request logging

--- Sources/Logging.swift
+++ Sources/Logging.swift
@@ -1,3 +1,31 @@
-protocol Logging {
+import Foundation
+
+// MARK: - Errors
+
+protocol ErrorLogging {
   func logError(error: ErrorType)
+}
+
+struct ErrorLogger: ErrorLogging {
+
+  func logError(error: ErrorType) {
+    NSLog("\(error)")
+  }
+}
+
+// MARK: - Requests
+
+protocol RequestLogging {
+  func logRequest(request: NSURLRequest)
+}
+
+struct RequestLogger: RequestLogging {
+
+  func logRequest(request: NSURLRequest) {
+    guard let URLString = request.URL?.absoluteString else {
+      return
+    }
+
+    NSLog("\(URLString)")
+  }
 }
