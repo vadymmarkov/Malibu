9c61b0b35e63a3e5612d66607d9ec2e8493716be
Vadym Markov <markov.vadym@gmail.com>
2016-06-02 20:43:12 +0200

    Refactor logging with new levels and loggers

--- Sources/Logging.swift
+++ Sources/Logging.swift
@@ -2,51 +2,113 @@ import Foundation
 
 // MARK: - Logger
 
+public enum LogLevel {
+  case None, Error, Info, Verbose
+}
+
 public class Logger {
 
-  public enum Level {
-    case Debug, Info, Error, Disabled
-  }
+  public var level: LogLevel = .None
+  public var errorLogger: ErrorLogging.Type = ErrorLogger.self
+  public var requestLogger: RequestLogging.Type = RequestLogger.self
+  public var responseLogger: ResponseLogging.Type = ResponseLogger.self
 
-  public var level: Level = .Disabled
-  public var errorLogger: ErrorLogging = ErrorLogger()
-  public var infoLogger: InfoLogging = InfoLogger()
-
-  var logErrors: Bool {
-    return level == .Error || level == .Debug
+  public var enabled: Bool {
+    return level != .None
   }
+}
 
-  var logInfo: Bool {
-    return level == .Info || level == .Debug
-  }
+public protocol Logging {
+  var level: LogLevel { get }
+  init(level: LogLevel)
 }
 
 // MARK: - Errors
 
-public protocol ErrorLogging {
+public protocol ErrorLogging: Logging {
   func logError(error: ErrorType)
 }
 
 public struct ErrorLogger: ErrorLogging {
 
+  public let level: LogLevel
+
+  public init(level: LogLevel) {
+    self.level = level
+  }
+
   public func logError(error: ErrorType) {
+    guard level != .None else {
+      return
+    }
+
     print("\(error)")
   }
 }
 
-// MARK: - Info
+// MARK: - Request
 
-public protocol InfoLogging {
-  func logRequest(request: NSURLRequest)
+public protocol RequestLogging: Logging {
+  func logRequest(request: Requestable, URLRequest: NSURLRequest)
 }
 
-public struct InfoLogger: InfoLogging {
+public struct RequestLogger: RequestLogging {
 
-  public func logRequest(request: NSURLRequest) {
-    guard let URLString = request.URL?.absoluteString else {
+  public let level: LogLevel
+
+  public init(level: LogLevel) {
+    self.level = level
+  }
+
+  public func logRequest(request: Requestable, URLRequest: NSURLRequest) {
+    guard let URLString = URLRequest.URL?.absoluteString else {
       return
     }
 
-    print("\(URLString)")
+    guard level == .Info || level == .Verbose else {
+      return
+    }
+
+    print("============================================")
+    print("\(request.method.rawValue) \(URLString)")
+
+    if level == .Verbose {
+      logDictionary(request.message.parameters)
+      logDictionary(request.message.headers)
+    }
+  }
+
+  func logDictionary(dictionary: [String: AnyObject]) {
+    guard let data = try? NSJSONSerialization.dataWithJSONObject(dictionary, options: .PrettyPrinted),
+      string = NSString(data: data, encoding: NSUTF8StringEncoding)
+      else {
+        return
+      }
+
+    print(string)
+  }
+}
+
+// MARK: - Response
+
+public protocol ResponseLogging: Logging {
+  func logResponse(response: NSHTTPURLResponse)
+}
+
+public struct ResponseLogger: ResponseLogging {
+
+  public let level: LogLevel
+
+  public init(level: LogLevel) {
+    self.level = level
+  }
+
+  public  func logResponse(response: NSHTTPURLResponse) {
+    guard level == .Info || level == .Verbose else {
+      return
+    }
+
+    print("Response status code: \(response.statusCode)")
+    print("============================================")
   }
 }
