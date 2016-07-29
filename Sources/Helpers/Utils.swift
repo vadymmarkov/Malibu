3ca9ba11adfa30a3465d33022a4d11dcdf9f9ab2
Khoa Pham <onmyway133@gmail.com>
2016-07-29 09:14:08 +0200

    Check for os name and os info

--- Sources/Helpers/Utils.swift
+++ Sources/Helpers/Utils.swift
@@ -24,4 +24,25 @@ struct Utils {
   static func filePath(name: String) -> String {
     return "\(Utils.storageDirectory)/\(name)"
   }
+
+  // MARK: - OS
+
+  static var osName: String {
+    #if os(iOS)
+      return "iOS"
+    #elseif os(tvOS)
+      return "tvOS"
+    #elseif os(watchOS)
+      return "watchOS"
+    #elseif os(OSX)
+      return "macOS"
+    #else
+      return "Unknown"
+    #endif
+  }
+
+  static var osInfo: String {
+    let version = NSProcessInfo.processInfo().operatingSystemVersion
+    return "\(osName) \(version.majorVersion).\(version.minorVersion).\(version.patchVersion)"
+  }
 }
