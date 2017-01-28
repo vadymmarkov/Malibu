16cfaf21dfeea35ad5df9d72a3b5e620d6a5b635
Vadym Markov <markov.vadym@gmail.com>
2017-01-28 16:14:51 +0100

    Introduce endpoint

--- Sources/Request/Schema.swift
+++ Sources/Request/Schema.swift
@@ -5,3 +5,9 @@ public protocol Schema {
   var sessionConfiguration: SessionConfiguration { get }
 }
 
+public protocol Endpoint: Requestable {
+
+  var path: URLStringConvertible { get }
+  var parameters: [String: Any] { get }
+  var headers: [String: String] { get }
+}
