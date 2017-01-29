0fd05935c4eb1d3620b0382149af32a3e5ba3274
Vadym Markov <markov.vadym@gmail.com>
2017-01-29 15:36:02 +0100

    Add default schema

--- Sources/Request/Schema.swift
+++ Sources/Request/Schema.swift
@@ -2,15 +2,29 @@ public protocol Schema {
 
   associatedtype EndpointType: Endpoint
 
-  var endpointType: EndpointType { get }
   var baseUrl: URLStringConvertible { get }
   var headers: [String: String] { get }
   var sessionConfiguration: SessionConfiguration { get }
 }
 
-public protocol Endpoint: Requestable {
+public protocol Endpoint {
 
-  var path: URLStringConvertible { get }
-  var parameters: [String: Any] { get }
-  var headers: [String: String] { get }
+  var request: Request { get }
+}
+
+// MARK: - Defaults
+
+extension Request: Endpoint {
+
+  public var request: Request {
+    return self
+  }
+}
+
+public struct DefaultSchema: Schema {
+
+  public typealias EndpointType = Request
+  public let baseUrl: URLStringConvertible = ""
+  public let headers: [String: String] = [:]
+  public let sessionConfiguration: SessionConfiguration = SessionConfiguration.default
 }
