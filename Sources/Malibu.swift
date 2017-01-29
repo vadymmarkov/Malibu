f40daec6f4517a8a48ba7ba64408cc61b3fbf065
Vadym Markov <markov.vadym@gmail.com>
2017-01-29 15:37:30 +0100

    Refactor backfoot surfer

--- Sources/Malibu.swift
+++ Sources/Malibu.swift
@@ -5,37 +5,14 @@ public enum Mode {
   case regular, partial, fake
 }
 
-// MARK: - Helpers
-
-var networkings = [String: Networking]()
-
 // MARK: - Vars
 
 public var mode: Mode = .regular
-public var backfootSurfer = Networking()
+public var backfootSurfer = Networking<DefaultSchema>(schema: DefaultSchema())
 public var parameterEncoders = [ContentType: ParameterEncoding]()
 public let logger = Logger()
 
 public let boundary = String(format: "Malibu%08x%08x", arc4random(), arc4random())
-
-// MARK: - Networkings
-
-public func register(_ name: String, networking: Networking) {
-  networking.requestStorage = RequestStorage(name: name)
-  networkings[name] = networking
-}
-
-public func unregister(_ name: String) {
-  guard let networking = networkings.removeValue(forKey: name) else {
-    return
-  }
-
-  networking.requestStorage.clear()
-}
-
-public func networking(_ name: String) -> Networking {
-  return networkings[name] ?? backfootSurfer
-}
 
 // MARK: - Storages
 
@@ -52,26 +29,6 @@ public func register(mock: Mock) {
 
 // MARK: - Requests
 
-public func GET(_ request: GETRequestable) -> Ride {
-  return backfootSurfer.GET(request)
-}
-
-public func POST(_ request: POSTRequestable) -> Ride {
-  return backfootSurfer.POST(request)
-}
-
-public func PUT(_ request: PUTRequestable) -> Ride {
-  return backfootSurfer.PUT(request)
-}
-
-public func PATCH(_ request: PATCHRequestable) -> Ride {
-  return backfootSurfer.PATCH(request)
-}
-
-public func DELETE(_ request: DELETERequestable) -> Ride {
-  return backfootSurfer.DELETE(request)
-}
-
-public func HEAD(_ request: HEADRequestable) -> Ride {
-  return backfootSurfer.HEAD(request)
+public func request(_ request: Request) -> Ride {
+  return backfootSurfer.execute(request)
 }
